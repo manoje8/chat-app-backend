d712217d6deb49f17a1bea2b17b56fa375d6bf24
Mano Deepan <manodeepan2001@gmail.com>
2024-09-26 16:20:53 +0530

    updated cors origin

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: process.env.CLIENT_URL,
         methods: ['GET', 'POST']
     }
 })
