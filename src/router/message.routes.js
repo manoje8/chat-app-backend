c3595773ed81ef9f3f6e7f8f77bde7a5e272989f
Mano Deepan <manodeepan2001@gmail.com>
2024-10-14 00:15:00 +0530

    Added delete controller

--- src/router/message.routes.js
+++ src/router/message.routes.js
@@ -4,7 +4,8 @@ import { verifyAccessToken } from "../utils/firebaseHelper.js";
 
 const messageRoutes = Router()
 
-messageRoutes.get("/:id", verifyAccessToken ,Message.getMessages)
+messageRoutes.get("/:id", verifyAccessToken ,Message.getMessagesById)
 messageRoutes.post("/send/:id", verifyAccessToken ,Message.sendMessage)
+messageRoutes.delete("/:id", Message.deleteMessageById)
 
 export default messageRoutes
